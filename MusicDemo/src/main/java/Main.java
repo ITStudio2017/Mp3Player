5cd5339013840f40cd0d0048248fe0901fec0f07
alex <330953853@qq.com>
2018-11-16 18:59:51 +0800

    adjust

--- MusicDemo/src/main/java/Main.java
+++ MusicDemo/src/main/java/Main.java
@@ -2,32 +2,12 @@ import javazoom.jl.decoder.JavaLayerException;
 import java.io.*;
 import java.util.concurrent.TimeUnit;
 
-class Myplayer implements Runnable{
+class Myplayer{
 //    static Player player;
-    static MyAdvancedPlayer player;
     static final String[] musicFileList = {
             "/Users/alex/Music/iTunes/iTunes Media/Music/毛不易/明日之子 第7期/消愁.mp3",
             "/Users/alex/Music/iTunes/iTunes Media/Music/买辣椒也用券/起风了（Cover 高橋優-《ヤキモチ》）/1-01 起风了（Cover 高橋優）.mp3",
     };
-    Myplayer(String musicFilePath) throws FileNotFoundException, JavaLayerException {
-        player = new MyAdvancedPlayer(new BufferedInputStream(new FileInputStream(new File(musicFilePath))));
-    }
-    public void play() {
-        try {
-            player.play();
-        } catch (JavaLayerException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void close(){
-        player.close();
-    }
-
-    public void run() {
-
-        play();
-    }
 }
 
 public class Main {
