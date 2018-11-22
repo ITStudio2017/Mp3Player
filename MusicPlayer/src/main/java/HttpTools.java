c852cc88d1eb6927156a4e04e2557eece8db5244
alex <330953853@qq.com>
2018-11-22 17:53:49 +0800

    完善下载功能， 增加在线听歌

--- MusicPlayer/src/main/java/HttpTools.java
+++ MusicPlayer/src/main/java/HttpTools.java
@@ -1,59 +1,77 @@
+import javazoom.jl.decoder.Bitstream;
+import javazoom.jl.decoder.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Vector;
 import java.io.*;
 
+
+
 public class HttpTools {
 
-    static String mainUrl = "https://music.itmxue.cn/api.php";
+    //音乐源
+    public enum Sources {netease,tencent,xiami,kugou}
+
+    //API接口
+    private   static String mainUrl = "https://music.itmxue.cn/api.php";
 
     //搜索功能
-    public static Vector<HashMap<String, String>> search(String keyWord, int page){
+    public static Vector<HashMap<String, String>> search(
+            String keyWord, int page){
+        //默认网易云搜索
+        return search(keyWord, page, Sources.netease.toString());
+    }
+
+    public static Vector<HashMap<String, String>> search(
+            String keyWord, int page, String source){
+
+        //构造post请求
         Vector<HashMap<String, String>> vector = new Vector<HashMap<String, String>>();
         List<NameValuePair> list = new ArrayList<NameValuePair>();
         list.add(new BasicNameValuePair("types","search"));
         list.add(new BasicNameValuePair("count","20"));
-        list.add(new BasicNameValuePair("source", "netease"));
+        list.add(new BasicNameValuePair("source", source));
         list.add(new BasicNameValuePair("pages", Integer.toString(page)));
         list.add(new BasicNameValuePair("name",keyWord));
+
+        //获取查询集
         JSONArray jsonArray = new JSONArray(doPost(mainUrl, list));
         if(jsonArray != null){
             for (int i = 0; i < jsonArray.length(); i++) {
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
                 HashMap<String, String> hashMap = new HashMap<String, String>();
-                hashMap.put("id",Integer.toString(jsonObject.getInt("id")));
-                hashMap.put("name",jsonObject.getString("name"));
-                hashMap.put("artist",jsonObject.getJSONArray("artist").getString(0));
-                hashMap.put("url_id",Integer.toString(jsonObject.getInt("url_id")));
-                hashMap.put("lyric_id",Integer.toString(jsonObject.getInt("lyric_id")));
-                hashMap.put("source",jsonObject.getString("source"));
-                hashMap.put("album",jsonObject.getString("album"));
-                try {
-                    //无封面pic_id = 0
-                    hashMap.put("pic_id",jsonObject.getString("pic_id"));
-                }catch (JSONException e){
-                    hashMap.put("pic_id","0");
-                }
+                hashMap.put("id",jsonObject.get("id").toString());
+                hashMap.put("name",jsonObject.get("name").toString());
+                hashMap.put("artist",jsonObject.getJSONArray("artist").get(0).toString());
+                hashMap.put("url_id",jsonObject.get("url_id").toString());
+                hashMap.put("lyric_id",jsonObject.get("lyric_id").toString());
+                hashMap.put("source",jsonObject.get("source").toString());
+                hashMap.put("album",jsonObject.get("album").toString());
+                hashMap.put("pic_id",jsonObject.get("pic_id").toString());
+
                 vector.add(hashMap);
             }
         }
@@ -61,64 +79,78 @@ public class HttpTools {
     }
 
     //下载歌曲
-    public static boolean downloadMusic(HashMap<String, String> music){
+    public static boolean downloadMusic(
+            HashMap<String, String> music){
+        //目标路径
         String targetPath = System.getProperty("user.dir") + "/downloads/" +
-                music.get("artist") + "/" + music.get("album") +
-                "/" + music.get("name") + "/" + music.get("name") + " - " + music.get("artist");
-        String fileName = music.get("name") + " - " + music.get("artist");
+                            music.get("artist") + "/" + music.get("album") +
+                            "/" + music.get("name") + "/" + music.get("name") +
+                            " - " + music.get("artist");
         boolean downloadFile = downloadMusicFile(music, targetPath + ".mp3");
         boolean downloadPic = downloadPic(music, targetPath + ".jpg");
         boolean downloadLrc = downloadLyric(music, targetPath + ".lrc");
 
         if(downloadFile && downloadPic && downloadLrc){
+            //下载成功
             return true;
         }
         return false;
     }
 
     //下载音乐文件
-    public static boolean downloadMusicFile(HashMap<String, String> hashMap, String targetPath){
-        //获取音乐url,此url短时间可访问下载，但我们的目的还是用于得到签名构造新url
+    private static boolean downloadMusicFile(
+            HashMap<String, String> hashMap, String targetPath){
+        //获取音乐url,此url可能为空
         List<NameValuePair> list = new ArrayList<NameValuePair>();
         list.add(new BasicNameValuePair("types", "url"));
         list.add(new BasicNameValuePair("id",hashMap.get("id")));
         list.add(new BasicNameValuePair("source",hashMap.get("source")));
         String url = new JSONObject(doPost(mainUrl,list)).getString("url");
-        return getForDownload(url, targetPath);
+        if(url.equals("")){
+            url = "https://music.163.com/song/media/outer/url?id=" + hashMap.get("id") + ".mp3";
+        }
 
-//        //获取对象存储中音乐的url，需要签名，短时间访问有效
-//        String requestUrl = "https://music.itmxue.cn/download";
-//        JSONObject jsonObject = new JSONObject();
-//        jsonObject.put("artist", hashMap.get("artist"));
-//        jsonObject.put("id", Integer.parseInt(hashMap.get("id")));
-//        jsonObject.put("name",hashMap.get("name"));
-//        jsonObject.put("source",hashMap.get("source"));
-//        jsonObject.put("url",url);
-//        JSONObject result = new JSONObject(doJsonPost(requestUrl,jsonObject));
-//
-//        //下载
-//        if(result.getInt("code") == 1){
-//            String musicUrl = result.getJSONObject("data").getString("url");
-//            return getForDownload(musicUrl, targetPath);
-//        }
-//        return false;
+        //获取对象存储中音乐的url，需要签名，短时间访问有效
+        String requestUrl = "https://music.itmxue.cn/download";
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("artist", hashMap.get("artist"));
+        jsonObject.put("id", hashMap.get("id"));
+        jsonObject.put("name",hashMap.get("name"));
+        jsonObject.put("source",hashMap.get("source"));
+        jsonObject.put("url",url);
+        JSONObject result = new JSONObject(doJsonPost(requestUrl,jsonObject));
+
+        //下载
+        if(result.getInt("code") == 1){
+            String musicUrl = result.getJSONObject("data").getString("url");
+            return getForDownload(musicUrl, targetPath);
+        }
+        return false;
     }
 
     //下载封面
-    public static boolean downloadPic(HashMap<String,String> hashMap, String targetPath){
+    private static boolean downloadPic(
+            HashMap<String,String> hashMap, String targetPath){
         List<NameValuePair> list = new ArrayList<NameValuePair>();
         list.add(new BasicNameValuePair("types","pic"));
         list.add(new BasicNameValuePair("id",hashMap.get("pic_id")));
-        list.add(new BasicNameValuePair("source",hashMap.get("netease")));
+        list.add(new BasicNameValuePair("source",hashMap.get("source")));
         JSONObject jsonObject = new JSONObject(doPost(mainUrl, list));
         String imgUrl = jsonObject.getString("url");
-        imgUrl = imgUrl.substring(0,imgUrl.length() - 14);
+        if(hashMap.get("source").toString().equals(Sources.netease.toString())){
+            //网易云封面url处理,下载高清版😄😄
+            imgUrl = imgUrl.split("\\?")[0];
+        }
+        if(hashMap.get("source").toString().equals(Sources.xiami.toString())){
+            imgUrl = imgUrl.split("@")[0];
+        }
         return getForDownload(imgUrl, targetPath);
 
     }
 
     //下载歌词
-    public static boolean downloadLyric(HashMap<String, String> hashMap, String targetPath) {
+    private static boolean downloadLyric(
+            HashMap<String, String> hashMap, String targetPath) {
         List<NameValuePair> list = new ArrayList<NameValuePair>();
         list.add(new BasicNameValuePair("types","lyric"));
         list.add(new BasicNameValuePair("id", hashMap.get("id")));
@@ -146,7 +178,8 @@ public class HttpTools {
     }
 
     //下载文件到指定目录
-    public static boolean getForDownload(String url, String targetPath){
+    private static boolean getForDownload(
+            String url, String targetPath){
         makeParentFolder(targetPath);
         CloseableHttpClient httpClient = HttpClients.createDefault();
         HttpGet httpGet = new HttpGet(url);
@@ -177,27 +210,33 @@ public class HttpTools {
     }
 
     //发送post请求 json类型
-    public static String doJsonPost(String url, JSONObject jsonObject){
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost(url);
-        System.out.println(jsonObject);
-        String s = null;
+    private static String doJsonPost(
+            String url, JSONObject jsonObject) {
+        StringEntity entity = null;
         try {
-            StringEntity entity = new StringEntity(jsonObject.toString());
-            httpPost.setEntity(entity);
-            httpPost.setHeader("Accept", "application/json");
-            httpPost.setHeader("Content-type", "application/json");
-            CloseableHttpResponse response = httpClient.execute(httpPost);
-            s = EntityUtils.toString(response.getEntity());
-            System.out.println("");
-        } catch (Exception e) {
+            entity = new StringEntity(jsonObject.toString());
+        } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
-        return s;
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpPost request = new HttpPost(url);
+        request.setHeader("Content-Type","application/json");
+        request.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:55.0) Gecko/20100101 Firefox/55.0");
+        request.setEntity(entity);
+        try {
+            HttpResponse response = httpClient.execute(request);
+            if (response.getStatusLine().getStatusCode() == 200){
+                return EntityUtils.toString(response.getEntity());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     //发送post请求 form类型
-    public static String doPost(String url, List<NameValuePair> list){
+    private static String doPost(
+            String url, List<NameValuePair> list){
         CloseableHttpClient httpClient = HttpClients.createDefault();
         String s = null;
         try {
@@ -221,7 +260,8 @@ public class HttpTools {
         return s;
     }
 
-    public static void makeParentFolder(String Path){
+    //生成目录
+    private static void makeParentFolder(String Path){
         File file = new File(Path);
         File parentFile =  new File(file.getParent());
         if(parentFile != null){
@@ -232,8 +272,38 @@ public class HttpTools {
         }
     }
 
-    public static void main(String[] args) {
-        Vector<HashMap<String, String>> music = search("龙舌兰",1);
-        downloadMusic(music.get(0));
+    public static void playMusic(String url){
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(url);
+        try {
+            HttpResponse response = httpClient.execute(httpGet);
+            StatusLine statusLine = response.getStatusLine();
+            int statusCode = statusLine.getStatusCode();//响应码
+            if (statusCode == 200) {
+                HttpEntity entity = response.getEntity();
+                try {
+                    MyAdvancedPlayer myAdvancedPlayer = new
+                            MyAdvancedPlayer(entity.getContent(), new Object());
+                    new Thread(myAdvancedPlayer).start();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    public static void main(String[] args) throws Exception {
+        String musicUrl = "https://m8.music.126.net/20181122181150/c7c3fd42eda56aec593f081d14f5091a/ymusic/a1cf/1381/deaa/fa8e84b88f0db4d7bc0da00983a516bf.mp3";
+        playMusic(musicUrl);
+        URL urlfile = new URL(musicUrl);
+        URLConnection con = urlfile.openConnection();
+        int b = con.getContentLength();
+        BufferedInputStream bis = new BufferedInputStream(con.getInputStream());
+        Bitstream bt = new Bitstream(bis);
+        Header h = bt.readFrame();
+        System.out.println(h.total_ms(b));
+    }
+
 }
--- MusicPlayer/src/main/java/Main.java
+++ MusicPlayer/src/main/java/Main.java
@@ -1,17 +1,16 @@
-import javazoom.jl.decoder.JavaLayerException;
-
-import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Vector;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException, JavaLayerException, InterruptedException {
-        PlayerThread playerThread = new PlayerThread(4,PlayerThread.musicFileList,Pattern.Sequence);
-        playerThread.start();
-        while (true){
-            System.out.println("音乐名：" + playerThread.musicFileList[playerThread.getNowMusic()]);
-            System.out.println("总时长：" + playerThread.getMusicTime());
-            System.out.println("当前播放时间：" + playerThread.getNowMusicTime());
-            Thread.sleep(400);
+    public static void main(String[] args) {
+        Vector<HashMap<String, String>> musicList =
+                HttpTools.search("可能否",1,
+                        HttpTools.Sources.netease.toString());
+
+        if(HttpTools.downloadMusic(musicList.get(0))){
+            System.out.println("下载成功");
         }
+
 
     }
 
